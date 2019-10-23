2567e67eefea50fd3b601576039128594185016e
benjaminbarbaric7 <55490606+benjaminbarbaric7@users.noreply.github.com>
2019-10-23 14:09:25 +0200

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    int A[6] = {2,34,1,67,99,7};
+    for(i=0;i<6;i++)
+    {
+                    cout<<"A["<<i<<"]="<<A[i]<<"  ";
+                    }
+                    cout<<endl;               
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
