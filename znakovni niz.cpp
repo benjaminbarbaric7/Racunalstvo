c36feeb1f26956111bb6c69c267cc44960f232d6
benjaminbarbaric7 <55490606+benjaminbarbaric7@users.noreply.github.com>
2019-10-30 13:38:46 +0100

    Add files via upload

---
+++ znakovni niz.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	int i;
+	char X[]="Ovo je znakovni niz";
+	for (i=0;i<20;i++){
+			cout<<"X["<<i<<"]="<<X[i]<<" "<<endl;
+	}
+	return 0;
+}
