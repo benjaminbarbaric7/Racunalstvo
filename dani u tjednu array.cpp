5290604c5338f43df65c63f5afea266b35d3fa1c
benjaminbarbaric7 <55490606+benjaminbarbaric7@users.noreply.github.com>
2019-10-30 13:43:56 +0100

    Add files via upload

---
+++ dani u tjednu array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	char dani[7][12] = {"nedjelja",
+						"ponedjeljak",
+						"utorak",
+						"srijeda",
+						"cetvrtak",
+						"petak",
+						"subota" };
+	cout<<"Kad ce taj "<<dani[5]<<endl;
+	return 0;
+}
