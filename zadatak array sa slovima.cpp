7b6f4eefd76181123262dbd42cfcd5f8f95c8c9e
benjaminbarbaric7 <55490606+benjaminbarbaric7@users.noreply.github.com>
2019-10-30 14:12:18 +0100

    Add files via upload

---
+++ zadatak array sa slovima.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+int i;
+char X[]="Otorinolaringologija";
+for(i=0;i<7;i++){
+	cout<<"X["<<i<<"]="<<X[i]<<" "<<endl;
+}	
+	return 0;
+}
