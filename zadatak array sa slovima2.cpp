543ba0012c1fdbeab114cafd5b3bd002b5ec7fd8
benjaminbarbaric7 <55490606+benjaminbarbaric7@users.noreply.github.com>
2019-10-30 14:16:46 +0100

    Add files via upload

---
+++ zadatak array sa slovima2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+//Ispisuje rijec otorinolaringologija unazad
+int main() 
+{
+int i;
+char X[]="Otorinolaringologija";
+for(i=19;i>=0;i--){
+	cout<<"X["<<i<<"]="<<X[i]<<" "<<endl;
+}	
+	return 0;
+}
