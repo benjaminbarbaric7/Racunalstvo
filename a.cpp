1f1fd00a6d9d513ffe08492f7f32e6fe4885ac22
benjaminbarbaric7 <55490606+benjaminbarbaric7@users.noreply.github.com>
2019-11-13 13:19:36 +0100

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,18 @@
+  
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	int x[5]={1,2,3,4,5};
+	
+	for(int i=0;i<5;i++)
+		cout<<"x["<<i<<"]="<<x[i]*2<<endl;
+		
+	system("pause");
+	return 0;
+	
+	
+}
