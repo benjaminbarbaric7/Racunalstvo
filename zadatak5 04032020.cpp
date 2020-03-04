d5f1fae659a6b8555bb8491067d7d5fbf3077461
benjaminbarbaric7 <55490606+benjaminbarbaric7@users.noreply.github.com>
2020-03-04 14:08:30 +0100

    Add files via upload

---
+++ zadatak5 04032020.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+
+int main() 
+{
+ofstream ispis ("D:\\Benjamin Barbariæ\\trokut.txt");
+ispis<<"    *    "<<endl;
+ispis<<"   ***   "<<endl;
+ispis<<"  *****  "<<endl;
+ispis<<" ******* "<<endl;
+	return 0;
+}
